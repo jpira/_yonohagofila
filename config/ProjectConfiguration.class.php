b8ee82232c5d4490321af73a0f929f9e3287195b
Juan Pablo Mejia <jpcardona@ibccodecontrol.com>
2013-05-24 08:11:41 -0500

    modificaciones de configuraciones por defecto

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,12 +1,16 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__) . '/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
-class ProjectConfiguration extends sfProjectConfiguration
-{
-  public function setup()
-  {
-    $this->enablePlugins('sfDoctrinePlugin');
-  }
+class ProjectConfiguration extends sfProjectConfiguration {
+
+    public function setup() {
+        $this->enablePlugins('sfDoctrinePlugin');
+
+        // Mensajes por defecto de los formularios
+        sfValidatorSchema::setDefaultMessage('required', 'Campo obligatorio');
+        sfValidatorSchema::setDefaultMessage('invalid', 'Campo inválido');
+    }
+
 }

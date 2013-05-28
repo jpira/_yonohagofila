4ce06a7eeac6bb37243ee2ba4c61769ed071f405
Juan Pablo Mejia <jpcardona@ibccodecontrol.com>
2013-05-28 11:10:15 -0500

    modificaciones implementando nuevo plugina admin generator

--- apps/public/modules/default/templates/indexSuccess.php
+++ apps/public/modules/default/templates/indexSuccess.php
@@ -9,11 +9,11 @@
                 estado=0;						   
                 $(".oculta").click(function () { 
                     if(estado==0) {
-                        $('#paraocultar').slideUp('fast');
+                        $('#paraocultar').slideUp('slow');
                         //                        $('#oculta').html('Reservar');
                         estado=1;
                     } else {
-                        $('#paraocultar').slideDown('fast');
+                        $('#paraocultar').slideDown('slow');
                         //                        $('#oculta').html('Reservar');
                         estado=0;
                     }
@@ -37,64 +37,59 @@
             -->
         </style>
     </head>
-
     <body>
         <?php
         $reservas = Doctrine_Query::create()->from('Reserva r')
                 ->orderBy('fecha_creacion desc')
                 ->execute();
-        
-        
-        
         $locales = Doctrine_Query::create()->from('Local l')
                 ->limit(3)
                 ->execute();
         $j = 0;
         ?>
-        
-        <?php foreach ($reservas as $reserva): ?>
         <table border="2" align="center">
             <tr>
                 <td>Nombre Lugar</td>
                 <td>Fecha Reserva</td>
                 <td>Estado</td>
             </tr>
-            <tr>
-                <td><?php echo $reserva->get('local_id') ?></td>
-                <td><?php echo $reserva->get('fecha_reserva') ?></td>
-                <td> </td>
-            </tr>
+            <?php foreach ($reservas as $reserva): ?>
+                <tr>
+                    <td><?php echo $reserva->get('Local')->get('nombre') ?></td>
+                    <td><?php echo $reserva->get('fecha_reserva') ?></td>
+                    <td><?php echo $reserva->get('estado') ?></td>
+                </tr>
+            <?php endforeach; ?>
         </table>
-        <?php endforeach; ?>
         <br><br><br>
-        <?php foreach ($locales as $local): ?>
-            <?php
-            $parametros = Doctrine_Query::create()->from('Parametro')
-                    ->where('local_id = ?', $local->get('id'))
-                    ->fetchOne();
-            ?>
-            <?php $j = $j + 1; ?>
-            <table  border="2" width="45%" heigh="50%" align="center">
-                <tr>
-                    <td rowspan="2"><?php echo image_tag('/uploads/imagen/' . $local->get('imagen'), array('size' => '120x80')) ?></td>
-                </tr>
-                <tr>
-                    <td>
-                        <h3><center><?php echo $local->get('nombre'); ?></center></h3>
-                        <?php echo $parametros->get('descripcion'); ?><br>
-                            <button class="oculta">Reservar</button></td>
-                </tr>
-            </table>
-        <?php endforeach; ?>
-        <br>
-            <div id="paraocultar" align="center" data-role="panel"> <br>
-                    <p>
-                        <form action="" method="POST">
-                            <h4>Datos de Reserva</h4>
-                            <?php echo $form; ?>
-                            <input type="submit" value="Reservar" />
-                        </form>
-                    </p>
-            </div>
-    </body>
+                    <?php foreach ($locales as $local): ?>
+                        <?php
+                        $parametros = Doctrine_Query::create()->from('Parametro')
+                                ->where('local_id = ?', $local->get('id'))
+                                ->fetchOne();
+                        ?>
+                        <?php $j = $j + 1; ?>
+                        <table  border="2" width="45%" heigh="50%" align="center">
+                            <tr>
+                                <td rowspan="2"><?php echo image_tag('/uploads/imagen/' . $local->get('imagen'), array('size' => '120x80')) ?></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <h3><center><?php echo $local->get('nombre'); ?></center></h3>
+                                    <?php echo $parametros->get('descripcion'); ?><br>
+                                        <button class="oculta">Reservar</button></td>
+                            </tr>
+                        </table>
+                    <?php endforeach; ?>
+                    <br>
+                        <div id="paraocultar" align="center" data-role="panel"> <br>
+                                <p>
+                                    <form action="" method="POST">
+                                        <h4>Datos de Reserva</h4>
+                                        <?php echo $form; ?>
+                                        <input type="submit" value="Reservar" />
+                                    </form>
+                                </p>
+                        </div>
+       </body>
 </html>

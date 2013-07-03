84aed01ca9c371853c767624a1c7918454def0c9
Jeison Pira <jpira@ibccodecontrol.com>
2013-07-03 07:55:30 -0500

    ordenando

---
+++ apps/public/modules/parciales/templates/_video.php
@@ -0,0 +1,29 @@
+<div class="element caja-presentacion primary-bg height2">
+    <input type="hidden" class="order" value="0">
+    <video class="video-presentacion" width="480" poster="images/preview-video.jpg" controls="controls" preload="auto">
+        <source src="../video/yonohagofilacom_mp4.mp4" type="video/mp4" />
+        <source src="../video/yonohagofilacom_ogg.ogg" type="video/ogg" />
+        <source src="../video/yonohagofilacom_webm.webm" type="video/webm" />
+        <embed src="../video/yonohagofilacom_swf.swf" type="application/x-shockwave-flash" width="480" height="360"></embed>
+    </video>
+    <h1 class="main-name">Aliquam adipiscing<br/>sem a quam eleifend<br/>vel dignissim metus<br/>ultrices volutpat.</h1>
+<!--<img src="http://placehold.it/900x950" class="full" alt="profile image">-->
+    <!--    <hr class="thick" />-->
+</div>
+<!--<div class="mano-acceso">
+    <img src="../img/mano-acceso.png" alt="Tu no haces fila">    
+</div>-->
+
+<!-- About -->
+<!--<div class="element about width3">
+    <input type="hidden" class="order" value="1">
+    <small>
+        Nice to meet you.
+    </small>
+    <h2 class="title">About Me
+    </h2>
+    <p>My name is <strong>Johnny</strong> and I am a web <strong>designer and developer</strong>. I possess a passion for building innovative and functional products.   
+    </p>
+    <p>You can put a description of yourself here as an introduction. Click around the navigation to see how content is presented.
+    </p>
+</div>-->

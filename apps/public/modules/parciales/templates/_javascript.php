5c78686b3ef19810a03c259192b3e0d891272e36
Jeison Pira <jpira@ibccodecontrol.com>
2013-07-20 11:35:34 -0500

    log out

--- apps/public/modules/parciales/templates/_javascript.php
+++ apps/public/modules/parciales/templates/_javascript.php
@@ -14,97 +14,99 @@ for ($i = 1; $i <= $locales; $i++):
                 titleFontColor: "#fff"
             });
 <?php endfor; ?>
-    //                setInterval(function() {
-    //                    g2.refresh(getRandomInt(120, 160));          
-    //                }, 2500);
-};
-$(function(){
+    setInterval(function() {
+<?php for ($i = 1; $i <= $locales; $i++): ?>
+                                    gauge2.refresh(getRandomInt(20, 50));          
+                                }, 2500);
+<?php endfor; ?>
+                    };
+                    $(function(){
       
-    var $container = $('#container');
+                        var $container = $('#container');
 
-    $container.isotope({
-        itemSelector : '.element'
-    });
+                        $container.isotope({
+                            itemSelector : '.element'
+                        });
       
       
-    var $optionSets = $('#options .option-set'),
-    $optionLinks = $optionSets.find('a');
+                        var $optionSets = $('#options .option-set'),
+                        $optionLinks = $optionSets.find('a');
 
-    $optionLinks.click(function(){
-        var $this = $(this);
-        // don't proceed if already selected
-        if ( $this.hasClass('selected') ) {
-            return false;
-        }
-        var $optionSet = $this.parents('.option-set');
-        $optionSet.find('.selected').removeClass('selected');
-        $this.addClass('selected');
+                        $optionLinks.click(function(){
+                            var $this = $(this);
+                            // don't proceed if already selected
+                            if ( $this.hasClass('selected') ) {
+                                return false;
+                            }
+                            var $optionSet = $this.parents('.option-set');
+                            $optionSet.find('.selected').removeClass('selected');
+                            $this.addClass('selected');
   
-        // make option object dynamically, i.e. { filter: '.my-filter-class' }
-        var options = {},
-        key = $optionSet.attr('data-option-key'),
-        value = $this.attr('data-option-value');
-        // parse 'false' as false boolean
-        value = value === 'false' ? false : value;
-        options[ key ] = value;
-        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-            // changes in layout modes need extra logic
-            changeLayoutMode( $this, options )
-        } else {
-            // otherwise, apply new options
-            $container.isotope( options );
-        }
+                            // make option object dynamically, i.e. { filter: '.my-filter-class' }
+                            var options = {},
+                            key = $optionSet.attr('data-option-key'),
+                            value = $this.attr('data-option-value');
+                            // parse 'false' as false boolean
+                            value = value === 'false' ? false : value;
+                            options[ key ] = value;
+                            if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+                                // changes in layout modes need extra logic
+                                changeLayoutMode( $this, options )
+                            } else {
+                                // otherwise, apply new options
+                                $container.isotope( options );
+                            }
         
-        return false;
-    });
+                            return false;
+                        });
 
       
-});   
+                    });   
             
-var editor;
+                    var editor;
     
-$(document).ready(function() {
+                    $(document).ready(function() {
 	 
-//    $("input.file").si();   
+                        //    $("input.file").si();   
 	 
-    // servira para editar los de tipo input text.
-    $('.text').editable('<?php echo url_for('@editaru?id=' . $sf_user->getAttribute('Usuario')->get('id')) ?>', {
-        indicator : 'Saving...', 
-        submit : 'OK'
-    });
+                        // servira para editar los de tipo input text.
+                        $('.text').editable('<?php echo url_for('@editaru?id=' . $sf_user->getAttribute('Usuario')->get('id')) ?>', {
+                            indicator : 'Saving...', 
+                            submit : 'OK'
+                        });
                 
-    $('.text2').editable('<?php echo url_for('@editaru2?id=' . $sf_user->getAttribute('Usuario')->get('id')) ?>', {
-        indicator : 'Saving...',                  
-        submit : 'OK'
-    });
+                        $('.text2').editable('<?php echo url_for('@editaru2?id=' . $sf_user->getAttribute('Usuario')->get('id')) ?>', {
+                            indicator : 'Saving...',                  
+                            submit : 'OK'
+                        });
     
-    $('.select').editable('<?php echo url_for('@cancelarr?id=' . $sf_user->getAttribute('Usuario')->get('id')) ?>', { 
-        data   : " {'1':'Cancelar'}",
-        type   : 'select',
-        submit : 'OK'
-    });     
+                        $('.select').editable('<?php echo url_for('@cancelarr?id=' . $sf_user->getAttribute('Usuario')->get('id')) ?>', { 
+                            data   : " {'1':'Cancelar'}",
+                            type   : 'select',
+                            submit : 'OK'
+                        });     
 
-    $('#reservass, #reservasss, #historialr, #historialrr, #alerta').dataTable(
-    {
-        "bScrollInfinite": true,
-        "bScrollCollapse": true,
-        "sScrollY": "300px"
+                        $('#reservass, #reservasss, #historialr, #historialrr, #alerta').dataTable(
+                        {
+                            "bScrollInfinite": true,
+                            "bScrollCollapse": true,
+                            "sScrollY": "300px"
        
-    }    
-);
-    $("#enviar").click(function(){
-        var formulario = $("#falerta").serializeArray();
-        $.ajax({
-            type: "POST",
-            dataType: 'json',
-            url: "<?php echo url_for('@alarma') ?>",
-            data: formulario
-        });
-    });
-    var options, a;
-    jQuery(function(){
-        //        options = { serviceUrl:'service/autocomplete.ashx' };
-        a = $('#busqueda').autocomplete();
-    });
-});
+                        }    
+                    );
+                        $("#enviar").click(function(){
+                            var formulario = $("#falerta").serializeArray();
+                            $.ajax({
+                                type: "POST",
+                                dataType: 'json',
+                                url: "<?php echo url_for('@alarma') ?>",
+                                data: formulario
+                            });
+                        });
+                        var options, a;
+                        jQuery(function(){
+                            //        options = { serviceUrl:'service/autocomplete.ashx' };
+                            a = $('#busqueda').autocomplete();
+                        });
+                    });
 </script>
